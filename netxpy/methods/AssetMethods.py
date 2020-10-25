7726d686e98717deff70e8b040cde4d5d3688768
Kevin Ford <kefo@3windmills.com>
2020-10-25 16:00:46 -0400

    Rewrite get_asset function to use get_assets_by_query

--- netxpy/methods/AssetMethods.py
+++ netxpy/methods/AssetMethods.py
@@ -8,7 +8,18 @@ class AssetMethods():
         return
     
     def get_asset(self, asset_id):
-        results = self.sm.search('assetId:' + str(asset_id))
+        # This performs a raw search against the facetsearch
+        # results = self.sm.search('assetId:' + str(asset_id))
+        query = [
+            {
+                "operator": "and",
+                "exact": {
+                    "field": "assetId",
+                    "value": str(asset_id)
+                }
+            }
+        ]
+        results = self.sm.get_assets_by_query(query)
         return results["results"][0]
 
     def get_asset_attributes(self, asset_id):
