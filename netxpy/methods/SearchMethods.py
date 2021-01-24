44efc2347754f4bcf3a1d677542908ef649570f4
Kevin Ford <kefo@3windmills.com>
2021-01-24 12:42:32 -0500

    Search solr, return results formatted as closely to NetX API response as possible.

--- netxpy/methods/SearchMethods.py
+++ netxpy/methods/SearchMethods.py
@@ -51,28 +51,74 @@ class SearchMethods():
             r["importDate_iso"] = self._millisecondsToIso(r["importDate"])
             r["modDate_iso"] = self._millisecondsToIso(r["modDate"])
         return results
-        
-    '''
+
     def get_assets_from_solr(self, queryHuman="", queryRaw="", start=0, count=101):
-        qplus = "&fq=assetId:*&rows=" + str(count) + "&start=" + str(start)
+        qplus = "&fq=assetId:*&fq=name:*&rows=" + str(count) + "&start=" + str(start)
         q = "q=" + queryHuman + qplus
         if queryRaw != "":
             q = queryRaw + qplus
-        
+
+        file_props = {
+            'ast_checksum_p': "checksum",
+        }
+        asset_props = {
+            'assetId': "id",
+            'name': "name",
+            'file': "fileName",
+            'ast_creationDate_d': "creationDate_iso",
+            'ast_modDate_d': "modDate_iso",
+            'ast_importDate_d': "importDate_iso"
+        }
+        folders_props = {
+            'ast_catParentPath_ps': "path",
+            'category': "id",
+            'categoryName': "name",
+            # 'categoryPath': "path",
+        }
+            
         assets = []
         results = self.netxconn.solrrequest(q)
         for d in results["response"]["docs"]:
+            asset = {}
             attributes = {}
-            folders = {}
             file = {}
-        '/''
-        for r in results["results"]:
-            r["creationDate_iso"] = self._millisecondsToIso(r["creationDate"])
-            r["importDate_iso"] = self._millisecondsToIso(r["importDate"])
-            r["modDate_iso"] = self._millisecondsToIso(r["modDate"])
-        '/''
-        return results
-    '''
+            folders = {'children': None}
+            for f in d:
+                if (f.startswith('attr_')):
+                    attrname = f.replace('attr_', '')
+                    attrname = attrname.replace('_', " ")
+                    attributes[attrname] = list(set(d[f]))
+                
+                elif (f.startswith('file') and f != "file"):
+                    attrname = f.replace('file', '')
+                    attrname = attrname.lower()
+                    file[attrname] = d[f]
+                
+                elif f in asset_props:
+                    if "Date" in f:
+                        asset[asset_props[f]] = d[f].replace('Z', '')
+                    else:
+                        asset[asset_props[f]] = d[f]
+                
+                elif f in file_props:
+                    file[file_props[f]] = d[f]
+                
+                elif f in folders_props:
+                    folders[folders_props[f]] = d[f][0]
+            
+            #asset["solr_doc"] = d
+            file["url"] = "/file/asset/" + str(d["assetId"]) + "/original/attachment"
+            
+            asset["creationDate"] = self._isoToMilliseconds(asset["creationDate_iso"])
+            asset["modDate"] = self._isoToMilliseconds(asset["modDate_iso"])
+            asset["importDate"] = self._isoToMilliseconds(asset["importDate_iso"])
+            
+            asset["file"] = file
+            asset["folders"] = [folders]
+            asset["attributes"] = attributes
+                
+            assets.append(asset)
+        return assets
         
     # This searches Solr fields and attributes.
     def raw_search(self, query_str, start=1, count=101):
