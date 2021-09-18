22f552daa349c3d90c9d41155d86f052f7a3015c
Kevin Ford <kefo@3windmills.com>
2021-09-18 17:31:31 -0400

    Add 'requests' as an install requirement.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-version = '0.0.8'
+version = '0.0.9'
 
 from setuptools import setup
 
@@ -10,7 +10,7 @@ setup(
     author_email = 'kford1@artic.edu',
     license = 'http://www.opensource.org/licenses/bsd-license.php',
     packages = ['netxpy', 'netxpy.methods'],
-    #install_requires = install_requires,
+    install_requires = ['requests'],
     description = 'Interact with NetX API',
     classifiers = [],
     test_suite = 'test',
